28d50362bc34cbaeef4975cd1c32d6f2108dc07e
Camila P. Dias <diascamila2005@gmail.com>
2025-07-08 20:20:01 -0300

    Add files via upload

---
+++ src/main/java/com/example/clinic/homeSystem/DoctorMyAppointmentsController.java
@@ -0,0 +1,25 @@
+package com.example.clinic.homeSystem;
+
+import com.example.clinic.Database.DoctorDatabase;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class DoctorMyAppointmentsController {
+    @FXML
+    protected void handleDatePicker(ActionEvent event) {
+        // ainda vou fazer
+
+        System.out.println("Date selected.");
+    }
+
+}
