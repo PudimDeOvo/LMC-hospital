7b3e1b770fa7c671add07b2602f38dbda8ae9d2d
Camila P. Dias <diascamila2005@gmail.com>
2025-07-11 18:33:08 -0300

    Update SceneManager.java

--- src/main/java/com/example/clinic/SceneManager.java
+++ src/main/java/com/example/clinic/SceneManager.java
@@ -42,16 +42,4 @@ public class SceneManager {
         }
     }
 
-    public static void switchScene(ActionEvent event, Parent root) {
-        try {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-
-            stage.show();
-        } catch (Exception e) {
-            System.err.println("Error switching scene with Parent root:");
-            e.printStackTrace();
-        }
-    }
 }
